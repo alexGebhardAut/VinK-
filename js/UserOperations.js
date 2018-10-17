5695ce2597fe4e923048a387af336377bb80636c
AlexTheDebuggerGebhard <wi13b001@technikum-wien.at>
2018-10-17 09:58:05 +0200

    Added login functionality

--- js/UserOperations.js
+++ js/UserOperations.js
@@ -10,7 +10,7 @@ function requiredField(fieldsArray) {
     return true;
 }
 
-// This function should make sure that certain fields in the form are required to be filled out 
+// This function should make sure that certain fields in the form are required to be filled out
 function signUpButton() {
     var requiredFields = [];
     requiredFields.push(document.getElementById("firstName"));
@@ -28,31 +28,31 @@ function signUpButton() {
         alert("You did not agree with everything");
     }
 
-/* When signing up, the website should check if all the required fields are filled out, 
-    and if the email, DOB, phone number, password, and repeat password meet our requirements */ 
-    else if (requiredField(requiredFields) === false || 
-                validateEmail() === false || 
-                validateDob() === false || 
+/* When signing up, the website should check if all the required fields are filled out,
+    and if the email, DOB, phone number, password, and repeat password meet our requirements */
+    else if (requiredField(requiredFields) === false ||
+                validateEmail() === false ||
+                validateDob() === false ||
                 validatePhoneNo() === false ||
-                validatePW() === false || 
+                validatePW() === false ||
                 matchPW() === false) {
         alert("Something got wrong")
     }
 
-// Users should check the Google ReCAPTCHA box before signing up to validate that they're real users  
+// Users should check the Google ReCAPTCHA box before signing up to validate that they're real users
     else if (document.getElementById("g-recaptcha-response").value.length === 0) {
         alert("Please, tick ReCAPTCHA checkbox")
     }
 
-// This should check if the email is already in use or not by calling the uniqueEmail function 
+// This should check if the email is already in use or not by calling the uniqueEmail function
     else if (uniqueEmail(requiredFields[2].value) === false) {
         alert("The email address is already used")
     }
 
-/* If everything is correctly filled out, a new user will be created 
+/* If everything is correctly filled out, a new user will be created
     The user's information includes the object address
     The news letter box can be checked or not (boolean) depending on the user's preference
-*/ 
+*/
     else{
         var user = new User(requiredFields[0].value,
                             requiredFields[1].value,
@@ -69,7 +69,7 @@ function signUpButton() {
                             document.getElementById("chxNewsletter").checked
             );
 
-// A new variable user is created and is pushed into the array userData 
+// A new variable user is created and is pushed into the array userData
         userData.push(user);
         alert("Congratulations, you have become a member!")
     }
@@ -79,14 +79,14 @@ function signUpButton() {
 function validateEmail() {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(String(document.getElementById("email").value).toLowerCase()) === false) {
-        document.getElementById("infoEmail").innerHTML = "This is not a valid email address"
+        document.getElementById("infoEmail").innerHTML = "This is not a valid email address";
         return false;
     }
     document.getElementById("infoEmail").innerHTML = "";
     return true;
 }
 
-// Validate if password fullfils criteria of minimum 8 characters long 
+// Validate if password fullfils criteria of minimum 8 characters long
 function validatePW() {
     var password = document.getElementById("password").value;
     if (password.length < 8) {
@@ -144,7 +144,7 @@ function validateDob() {
     }
 }
 
-/* Check if the email address is already stored in our system. 
+/* Check if the email address is already stored in our system.
     Parameter email
     Return value is boolean */
 function uniqueEmail(email) {
@@ -157,4 +157,37 @@ function uniqueEmail(email) {
         }
     }
     return true;
+}
+
+function showPwFuncDown() {
+    document.getElementById('password').type = "text";
+}
+
+function showPwFuncUp() {
+    document.getElementById('password').type = "password";
+}
+
+function loginButton() {
+    var output;
+    var fields = [document.getElementById("email").value, document.getElementById("password").value];
+
+    if (requiredField(fields) === false)
+        output = "Please fill out login and password";
+    else {
+        var tempPos = -1;
+        for (var i = 0; i < userData.length; i++) {
+            if (fields[0] === userData[i].email) {
+                if (fields[1] === userData[i].password) {
+                    output = "login correct"; tempPos = i; break;
+                } else {
+                    output = "Something got wrong"; tempPos = i; break;
+                }
+            }
+        }
+
+        if (tempPos === -1) {
+            output = "The given user is not registered";
+        }
+    }
+    alert(output);
 }
